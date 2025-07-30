962365a27505cf35e4a7d545bdfb8c7b00d6e225
swavi89 <swaviman89@gmail.com>
2025-07-30 23:13:37 +0530

    Rest API Complete

--- index.js
+++ index.js
@@ -1,25 +1,81 @@
-import express from 'express'
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/product.model.js";
 
-const app = express()
-
-
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello from node API Swavi");
+  res.send("Hello from node API Swavi");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-mongoose.connect("mongodb+srv://admin:RX4KYdCaN4o54jjm@backenddb.papqtvv.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
-.then(() => {
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/product", async (req, res) => {
+  // console.log(req.body);
+  // res.send(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:RX4KYdCaN4o54jjm@backenddb.papqtvv.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
     console.log("Connected to MongoDB");
     app.listen(3000, () => {
-        console.log("Server is running on port 3000");
+      console.log("Server is running on port 3000");
     });
-})
-.catch(() => {
+  })
+  .catch(() => {
     console.log("Connection failed");
-});
-
-
-
+  });
