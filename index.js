59acf536701ddfce8a09fccf653036fa0b16e32b
swavi89 <swaviman89@gmail.com>
2025-07-30 19:56:25 +0530

    Implement mongodb, mongoose and connect with database

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
 const app = express()
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+
 
 app.get("/", (req, res) => {
-    res.send("Hello from node API");
+    res.send("Hello from node API Swavi");
 });
+
+
+mongoose.connect("mongodb+srv://admin:RX4KYdCaN4o54jjm@backenddb.papqtvv.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+})
+.catch(() => {
+    console.log("Connection failed");
+});
+
+
+
