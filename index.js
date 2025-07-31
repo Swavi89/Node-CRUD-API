982d7109138972c30338957b5db6423bb035a31e
swavi89 <swaviman89@gmail.com>
2025-08-01 00:52:27 +0530

    Complete with routes and controllers and remove all logic from index js file

--- index.js
+++ index.js
@@ -1,71 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use("/api/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from node API Swavi");
 });
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/api/product", async (req, res) => {
-  // console.log(req.body);
-  // res.send(req.body);
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 mongoose
   .connect(
